ee36c038973a1bb56dc65eaa413ce6c8894b30e6
ThangKa <hothang2262004@gmail.com>
2025-02-24 23:31:54 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+//()=>{}
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
+
+
+  const myFunction = () => {
+    console.log('thangka')
+  }
+  myFunction();
 
   return (
     <>
